262d9a31945fc25457c70cb8b9a0d8316c66ff2d
VENKATSAIDASARI <98133671+VENKATSAIDASARI@users.noreply.github.com>
2022-04-06 16:31:08 -0500

    dog class added

--- routes/dog.js
+++ routes/dog.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-res.render('index',{title: 'Rams '});
+res.render('dog',{title: 'Search Results Dog '});
 });
 
 module.exports = router;
